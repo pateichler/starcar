cc88f7a06b9f27f8a1396b887f92be02e1ac4300
Patrick Eichler <pateichler12@gmail.com>
2025-03-14 15:32:05 -0500

    Added CLI tool to recalculate mission distances
    This tool is introduce to fix previous missions that were recording incorrect distances.

--- services/backend/cli.py
+++ services/backend/cli.py
@@ -1,9 +1,11 @@
 import secrets
 
 from flask.cli import FlaskGroup
+import click
 
 from starcar import app, db
-from starcar.models import SitePassword
+from starcar.models import SitePassword, TelemetryData
+from starcar.utils import get_telem_dist
 
 cli = FlaskGroup(app)
 
@@ -30,6 +32,33 @@ def seed():
     db.session.commit()
 
     print(f'Generated temporary password: {temp_pass}')
+
+
+@cli.command("recalc-dist")
+@click.argument("mission_id")
+def recalculate_mission_dist(mission_id: int):
+    total_telem = db.session.scalars(
+        db.select(TelemetryData)
+        .filter_by(data_id=mission_id)
+        .order_by(TelemetryData.time)
+    ).all()
+
+    last_telem = None
+    total_dist = 0
+    for t in total_telem:
+        dist = 0
+        
+        if last_telem is not None:
+            dist = get_telem_dist(
+                last_telem.latt, last_telem.lng, t.latt, t.lng
+            )
+
+        t.dist = dist
+        total_dist += dist
+        last_telem = t
+
+    db.session.commit()
+    print(f'Recalculated distances for mission {mission_id}\nTotal distance: {total_dist:.3f}km')
 
 
 if __name__ == "__main__":
